9789f3ad9270fe25eb55d0ff48eeb0f216a28ae6
VeenaSaranala20 <162396891+VeenaSaranala20@users.noreply.github.com>
2025-09-02 21:24:36 +0530

    Create AuthUserModel.php

---
+++ AuthUserModel.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class AuthUserModel extends Model {
+    protected $table = 'auth_user';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['email', 'first_name', 'last_name', 'password'];
+}
